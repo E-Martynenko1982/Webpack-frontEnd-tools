be3e681510bef4d19da332718d2f56346ee90e73
E-Martynenko1982 <motor8103@gmail.com>
2024-08-07 13:36:47 +0300

    update

--- lesson-6-webpack-loaders/task1/webpack.config.js
+++ lesson-6-webpack-loaders/task1/webpack.config.js
@@ -8,8 +8,12 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /.css$/,
-        use: ['style-loader', 'css-loader']
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /.(jpg|png)$/,
