07727e8f82b312b97057c5238bb9e298c350c5ec
E-Martynenko1982 <motor8103@gmail.com>
2024-08-07 17:02:50 +0300

    update

---
+++ lesson-6-webpack-loaders/task2/webpack.config.js
@@ -0,0 +1,16 @@
+
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: ['babel-loader']
+      }
+    ],
+  }
+};
