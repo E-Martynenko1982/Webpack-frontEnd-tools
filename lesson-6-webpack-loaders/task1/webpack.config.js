e1aa52d4564f4ef260d0accc849b771a48ef032c
E-Martynenko1982 <motor8103@gmail.com>
2024-08-07 13:39:17 +0300

    update

--- lesson-6-webpack-loaders/task1/webpack.config.js
+++ lesson-6-webpack-loaders/task1/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
           {
             loader: 'url-loader',
             options: {
-              limit: 8,
+              limit: 8192,
               name: '[name].[ext]',
               outputPath: 'images'
             },
