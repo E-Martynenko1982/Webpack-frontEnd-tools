3e89cfd85b05e614dff9db4224884f3612e8f3da
E-Martynenko1982 <motor8103@gmail.com>
2024-08-07 13:26:09 +0300

    update

---
+++ lesson-6-webpack-loaders/task1/webpack.config.js
@@ -0,0 +1,29 @@
+const { rules } = require("eslint-config-airbnb-base");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /.(jpg|png)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: '[name].[ext]',
+              outputPath: 'images'
+            },
+          },
+        ],
+      }
+    ],
+  }
+};
